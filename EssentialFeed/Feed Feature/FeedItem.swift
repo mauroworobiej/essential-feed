9430585bec15214979180010cd4fd397561958f9
Mauro Worobiej <mauro.worobiej@gmail.com>
2021-08-25 10:19:32 -0300

    Create factory method to simplify test setup

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -13,11 +13,11 @@ public struct FeedItem: Equatable {
     public let location: String?
     public let imageURL: URL
     
-    public init(id: UUID, description: String?, location: String?, image: URL) {
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
         self.id = id
         self.description = description
         self.location = location
-        self.imageURL = image
+        self.imageURL = imageURL
     }
 }
 
